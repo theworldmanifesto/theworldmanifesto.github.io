ea0469717ae727cb41aecae2fa14f05353a3d39e
theworldmanifesto <theworldmanifesto@gmail.com>
2026-08-02 07:55:57 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'el': { home: '🏠ΑΡΧΙΚΗ' },
         'hu': { home: '🏠FŐOLDAL' },
         'cs': { home: '🏠DOMŮ' },
-        'sv': { home: '🏠HEM' },
+        'sv': { home: '⌂HEM' },
         'bg': { home: '🏠НАЧАЛО' },
         'no': { home: '🏠HJEM' },
         'da': { home: '🏠HJEM' },
